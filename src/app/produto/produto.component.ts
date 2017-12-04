b1efeb419b09f0d0e945386bc54bd7b71806e039
Caio Nardi <caio@pagpouco.com>
2017-12-04 06:28:11 -0200

    Produto - view, e chamadas do service

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../produto.service';
+import {Produto} from '../produto';
+
+
+export class Parametros {
+  pagina: number;
+  itens_por_pagina: number;
+}
+
 
 @Component({
   selector: 'app-produto',
@@ -7,9 +16,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoComponent implements OnInit {
 
-  constructor() { }
+  public produtos: Produto[];
+  public pagina_atual: number;
+  public ultima_pagina: number;
+  public opts_itens_por_pagina;
+  public parametros:Parametros;
 
-  ngOnInit() {
+  constructor(
+      private produtoService: ProdutoService
+  ) {
+    this.opts_itens_por_pagina = [2, 3, 4, 5];
+    this.parametros = new Parametros();
+    this.parametros.pagina = 1;
+    this.parametros.itens_por_pagina = 3;
   }
 
+  ngOnInit() { 
+    this.lista();
+  }
+
+  listaProdutos(pagina) {
+    this.parametros.pagina = pagina;
+    this.lista();
+  }
+
+  listaProdutosP(itens_por_pagina) {
+    this.parametros.itens_por_pagina = itens_por_pagina.value;
+    this.lista();
+  }
+
+  lista() {
+    this.produtoService.get(this.parametros).subscribe((r) => {
+      this.produtos = r['data'];
+      this.pagina_atual = r['current_page'];
+      this.ultima_pagina = r['last_page'];
+    }, (f) => {
+      console.error(f);
+    });
+  }
 }
